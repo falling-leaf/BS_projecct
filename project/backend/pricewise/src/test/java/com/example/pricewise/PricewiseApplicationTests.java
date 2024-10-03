73ad833abb55fa9f35f70bb785e88a4fccbe1d59
falling-leaf <1445675246@qq.com>
2024-10-03 19:58:34 +0800

    后端基本框架

---
+++ project/backend/pricewise/src/test/java/com/example/pricewise/PricewiseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.pricewise;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PricewiseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
